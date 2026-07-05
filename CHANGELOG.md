9d828c1315d68ee894bcbbef5fe65a16fc3d7fe5
Emanuel <92552607+Manolo789@users.noreply.github.com>
2026-07-05 12:55:22 -0300

    Alteração no sistema de dependências do viewer.py

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,10 +1,15 @@
 CHANGELOG
 
+[2026/07/05 00:36]:
+Implementação de motores gráficos 3D adicionais:
+-Matplotlib foi descontinuado
+-PyVista definido como padrão
+-Trimesh implementado
+-Engine gráfica própria implementada com PySide6+OpenGL
 
+[2026/07/04 19:10]:
 
-\[2026/07/04 19:10]:
-
-\-Criação do arquivo .gitignore
+-Criação do arquivo .gitignore
 
 Bugs:
 -Modificação na função rebuild() em document.py para corrigir o erro: ao atualizar a resolução, a árvore de features continuava com a resolução anterior. Para corrigir isso, foi adicionada a condição da resolução atual ser diferente de uma variável temporária.
@@ -13,6 +18,6 @@ Bugs:
 Otimização:
 -Alteração do arquivo mesher.py para melhor performace.
 
-\-Ateração de 'array\_polar' por repetição de domínio.
+-Ateração de 'array\_polar' por repetição de domínio.
 -Adição da função pruned\_margin em conjunto com a otimização em mesher.py. Tal função ficará em sdf.py
 
--- parametricus/viewer.py
+++ parametricus/viewer.py
@@ -7,29 +7,28 @@ normal e eixos em escala real. Também salva imagens (PNG) do modelo.
 
 from __future__ import annotations
 
-import numpy as np
-from .viewer_backend.pyvista import show_pyvista
-from .viewer_backend.trimesh import show_trimesh
-from .viewer_backend.pyside6 import show_pyside6
-from .viewer_backend.mpl import show_mpl
 from .mesher import Mesh
 
 
 def show_mesh(mesh: Mesh, title: str = "parametricus", color: str = "#4a90d9",
               save_path: str | None = None, elev: float = 22, azim: float = -60,
               show: bool = True, engine="default") -> None:
-    common = {"mesh": mesh, "title":title, "color":color, "save_path":save_path, "show":show}
+    common = {"mesh": mesh, "title": title, "color": color, "save_path": save_path, "show": show}
     
     if (engine == "default") or (engine == "pyvista"):
+        from .viewer_backend.pyvista import show_pyvista
         return show_pyvista(**common)
         
     elif engine == "trimesh":
+        from .viewer_backend.trimesh import show_trimesh
         return show_trimesh(**common)
 
     elif engine == "pyside6":
+        from .viewer_backend.pyside6 import show_pyside6
         return show_pyside6(**common)
     
     elif (engine == "old") or (engine == "matplotlib"): # Descontinuado
+        from .viewer_backend.mpl import show_mpl
         return show_mpl(elev=elev, azim=azim, **common)
 
     else:
